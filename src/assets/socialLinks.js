0a83bcfeb56eff8500175208f1d0ef5b0eaa298e
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2026-01-15 00:30:32 +0530

    Assests

---
+++ src/assets/socialLinks.js
@@ -0,0 +1,31 @@
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
+
+const socialLinks = [
+  {
+    id: 1,
+    icon: FaGithub,
+    href: "https://github.com/SurajCh6613",
+    label: "GitHub",
+  },
+  {
+    id: 2,
+    icon: FaLinkedin,
+    href: "https://www.linkedin.com/in/surajch6613/",
+    label: "LinkedIn",
+  },
+  {
+    id: 3,
+    icon: SiLeetcode,
+    href: "https://leetcode.com/u/Surajch6613/",
+    label: "Leetcode",
+  },
+  {
+    id: 4,
+    icon: FaTwitter,
+    href: "https://x.com/Surajch6613",
+    label: "Twitter",
+  },
+];
+
+export default socialLinks;
